6174e2eeb2b34de28015d4a2be272457cf4d9fc2
corbinpro <Cdprobasco@gmail.com>
2024-09-19 11:15:03 -0600

    prep assignment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is you numeric grade?");
+        string score_input = Console.ReadLine();
+        int score = int.Parse(score_input);
+
+        if (score >= 90)
+        {
+            Console.WriteLine("Your letter grade is: A \nYou passed");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("Your letter grade is: B \nYou passed");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("Your letter grade is: C\nYou passed");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("Your letter grade is: D\nYou failed");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is: F\nYou failed");
+        }
     }
 }
