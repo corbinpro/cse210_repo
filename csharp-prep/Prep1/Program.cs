3428204b933a5e0cb52e70a223700703f618de0c
corbinpro <Cdprobasco@gmail.com>
2024-09-19 10:26:47 -0600

    prep1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("First name?");
+        string first = Console.ReadLine();
+        Console.WriteLine("Last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {first} {last}.");
     }
 }
