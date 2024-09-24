ca186005ed1c965e5c9bdc6cdc678f33ff251e4a
corbinpro <Cdprobasco@gmail.com>
2024-09-24 14:50:25 -0600

    guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_num = randomGenerator.Next(1,101);
+        Console.WriteLine("I have a number between 1 and 100. Can you guess it?");
+        int guess = 0;
+        while (guess != magic_num)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = Convert.ToInt32(Console.ReadLine());
+            if (guess < magic_num)
+            {
+                Console.WriteLine("Too low. Try again.");
+            }
+            else if (guess > magic_num)
+            {
+                Console.WriteLine("Too high. Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the number!");
+            }
+        }
+
     }
 }
