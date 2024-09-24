928fbeca642a53abd25e94c1ef28c1bb5ef107ab
corbinpro <Cdprobasco@gmail.com>
2024-09-24 08:46:26 -0600

    in class activity

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-        
+        Console.WriteLine("What is the name of author of your favorite quote?");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is the quote?");
+        string quote = Console.ReadLine();
+
+        Console.WriteLine($"\"{quote}\" \n- {name}");   
     }
 }
