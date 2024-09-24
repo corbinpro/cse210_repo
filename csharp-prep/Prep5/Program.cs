af441774dc37d574970ad968aee057a9860d0d0c
corbinpro <Cdprobasco@gmail.com>
2024-09-24 15:55:55 -0600

    prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int user_number = PromptUserNumber();
+        int squared_number = SquareNumberO(user_number);
+        DisplayResult(name, user_number, squared_number);
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Enter your favorite number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumberO(int user_number)
+    {
+        int number = user_number;
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int user_number, int squared_number)
+    {
+        Console.WriteLine(name + ", the square of your favorite number is " + squared_number);
+
+    }
     }
 }
