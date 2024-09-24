97bef80fef90a25dfefe9a958e01d870ee618083
corbinpro <Cdprobasco@gmail.com>
2024-09-24 15:34:53 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List<int> numbers = new List<int>();
+        int user_input = 1;
+        float total = 0;
+
+        do 
+        {
+            Console.WriteLine("Enter a number (0 to quit): ");
+            user_input = Convert.ToInt32(Console.ReadLine());
+            numbers.Add(user_input);
+
+        } while (user_input != 0);
+
+        numbers.RemoveAt(0);
+        
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine("The average of the numbers is: " + total / numbers.Count);
+
     }
 }
