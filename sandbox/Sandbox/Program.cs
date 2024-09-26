0b234327a38a2b2451a3761f12640321fb1ed34b
corbinpro <Cdprobasco@gmail.com>
2024-09-26 08:53:21 -0600

    grocery list

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,14 +1,29 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the name of author of your favorite quote?");
-        string name = Console.ReadLine();
-        Console.WriteLine("What is the quote?");
-        string quote = Console.ReadLine();
+        List<string> items = new List<string>();
+        string user_input = "";
+        while (user_input != "q")
+        {
+            Console.WriteLine("Enter an item (q to quit): ");
+            user_input = Console.ReadLine();
+            items.Add(user_input);
+        }
+        items.RemoveAt(items.Count - 1);
+        DisplayItems(items); 
+    }
+    static void DisplayItems(List<string> items)
+    {
+        int number_items = items.Count;
+        Console.WriteLine("Grocery List: "+"("+ number_items + " items)");
+        foreach (string item in items)
+        {
+            Console.WriteLine(item);
+        }
 
-        Console.WriteLine($"\"{quote}\" \n- {name}" );   
     }
 }
