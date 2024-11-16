ebd0a6ce26ae31f5db41682778e6b64a0157bdb0
corbinpro <Cdprobasco@gmail.com>
2024-11-15 17:27:46 -0700

    shapes n stuff

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        //rectangle
+        Rectangle rectangle = new Rectangle(5,10);
+        rectangle._SetColor("blue");
+        Console.WriteLine(rectangle._GetArea());
+        Console.WriteLine(rectangle._GetColor());
+
+        //square
+        Square square = new Square(5);
+        square._SetColor("green");
+        Console.WriteLine(square._GetArea());
+        Console.WriteLine(square._GetColor());
+
+        //circle
+        Circle circle = new Circle(5);
+        circle._SetColor("yellow");
+        Console.WriteLine(circle._GetArea());
+        Console.WriteLine(circle._GetColor());
     }
+}
+
+
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape()
+    {
+        _color = "red";
+    }
+
+    public void _SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public string _GetColor()
+    {
+        return _color;
+    }
+
+    public abstract double _GetArea();
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -59,9 +59,5 @@ class Program
             }
         }
     }
-
-
-
-
     
 }
